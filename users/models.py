eadd5ac3ee465c80d69a776d410c53527d2b8851
bherych <bogdangerych@gmail.com>
2025-11-03 13:37:16 +0200

    made user model and serializer

--- users/models.py
+++ users/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class User(User):
+    bio = models.TextField(blank=True)
+    country = models.CharField(max_length=100, blank=True)
+
+    def __str__(self):
+        return self.username
+
