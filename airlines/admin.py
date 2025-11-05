70ba7f0ec7a32607cee13a4cfc8581ec2f8a4174
bherych <bogdangerych@gmail.com>
2025-11-05 13:33:05 +0200

    added swagger, fixed admins

--- airlines/admin.py
+++ airlines/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from models import Airline, Airplane, Country, Airport
+from .models import Airline, Airplane, Country, Airport
 
 admin.site.register(Airline)
 admin.site.register(Airplane)
--- flights/admin.py
+++ flights/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from models import Flight, Ticket
+from .models import Flight, Ticket
 
 admin.site.register(Flight)
 admin.site.register(Ticket)
