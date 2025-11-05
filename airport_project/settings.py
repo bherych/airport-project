2b6045d68abdc19d2022f9ebe1f1a422dea74e77
bherych <bogdangerych@gmail.com>
2025-11-05 11:37:34 +0200

    added new db and registered admins

--- airport_project/settings.py
+++ airport_project/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'airport_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'airport_project',
+        'USER': 'postgres',           
+        'PASSWORD': 'Calculator-2021',
+        'HOST': 'localhost',         
+        'PORT': '5432',    
     }
 }
 
