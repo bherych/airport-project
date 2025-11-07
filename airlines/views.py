4dfb07cd82189012ad2a95f3c3a98e9ac254894e
bherych <bogdangerych@gmail.com>
2025-11-07 17:26:20 +0200

    implemented views for airlines and removed shell_test

--- airlines/views.py
+++ airlines/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Airline, Airplane, Airport, Country
+from .serializers import AirlineSerializer, AirplaneSerializer, AirportSerializer, CountrySerializer
 
-# Create your views here.
+class AirlineListCreateView(generics.ListCreateAPIView):
+    queryset = Airline.objects.all()
+    serializer_class = AirlineSerializer
+
+class AirplaneListCreateView(generics.ListCreateAPIView):
+    queryset = Airplane.objects.all()
+    serializer_class = AirplaneSerializer
+
+class AirportListCreateView(generics.ListCreateAPIView):
+    queryset = Airport.objects.all()
+    serializer_class = AirportSerializer
+
+class CountryListCreateView(generics.ListCreateAPIView):
+    queryset = Country.objects.all()
+    serializer_class = CountrySerializer
+
+class AirlineDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Airline.objects.all()
+    serializer_class = AirlineSerializer
+
+class AirplaneDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Airplane.objects.all()
+    serializer_class = AirplaneSerializer
+
+class AirportDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Airport.objects.all()
+    serializer_class = AirportSerializer
+
+class CountryDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Country.objects.all()
+    serializer_class = CountrySerializer
--- airport_project/urls.py
+++ airport_project/urls.py
@@ -19,6 +19,16 @@ from django.urls import path
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from airlines.views import (
+    AirlineListCreateView,
+    AirplaneListCreateView,
+    AirportListCreateView,
+    CountryListCreateView,
+    AirlineDetailView,
+    AirplaneDetailView,
+    AirportDetailView,
+    CountryDetailView,
+)   
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -36,4 +46,6 @@ urlpatterns = [
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('admin/', admin.site.urls),
+    path('airlines/', AirlineListCreateView.as_view(), name='airline-list-create'),
+    path('airlines/<int:pk>/', AirlineDetailView.as_view(), name='airline-detail'),
 ]
