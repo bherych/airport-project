cac2b118d48ae012ca705c06c05048d26c230d68
bherych <bogdangerych@gmail.com>
2025-11-24 10:12:42 +0200

    changed access for creating tickets

--- flights/views.py
+++ flights/views.py
@@ -6,6 +6,7 @@ from rest_framework.permissions import IsAuthenticated
 from django_filters.rest_framework import DjangoFilterBackend
 from .filters import FlightFilter
 from airport_project.permissions import ReadOnlyOrIsAdmin, IsOwnerOrAdmin
+from rest_framework.exceptions import PermissionDenied
 
 
 logger = logging.getLogger(__name__)
@@ -60,57 +61,30 @@ class FlightViewSet(viewsets.ModelViewSet):
 class TicketViewSet(viewsets.ModelViewSet):
     queryset = Ticket.objects.all()
     serializer_class = TicketSerializer
-    permission_classes = [IsAuthenticated, IsOwnerOrAdmin]
+    permission_classes = [IsAuthenticated]
     filter_backends = [DjangoFilterBackend]
     filterset_fields = ['status', 'user', 'flight']
-
 
     def get_queryset(self):
         user = self.request.user
         if user.is_staff:
             return Ticket.objects.all()
         return Ticket.objects.filter(passenger=user)
-        
-
-    def list(self, request, *args, **kwargs):
-        logger.info("Getting all tickets request")
-        try:
-            return super().list(request, *args, **kwargs)
-        except Exception as e:
-            logger.error(f"Error retrieving tickets: {e}")
-            raise
-
-    def retrieve(self, request, *args, **kwargs):
-        logger.info(f"Retrieving details of ticket with id={kwargs.get('pk')}")
-        try:
-            return super().retrieve(request, *args, **kwargs)
-        except Exception as e:
-            logger.error(f"Error retrieving ticket with id={kwargs.get('pk')}: {e}")
-            raise
 
     def create(self, request, *args, **kwargs):
-        logger.info(f"Creating new ticket: {request.data}")
-        try:
-            return super().create(request, *args, **kwargs)
-        except Exception as e:
-            logger.error(f"Error creating ticket: {e}")
-            raise
+        if not request.user.is_staff:
+            raise PermissionDenied("You cannot create tickets manually.")
+        return super().create(request, *args, **kwargs)
 
     def update(self, request, *args, **kwargs):
-        logger.info(f"Updating ticket with id={kwargs.get('pk')}: {request.data}")
-        try:
-            return super().update(request, *args, **kwargs)
-        except Exception as e:
-            logger.error(f"Error updating ticket with id={kwargs.get('pk')}: {e}")
-            raise
+        if not request.user.is_staff:
+            raise PermissionDenied("You cannot update tickets.")
+        return super().update(request, *args, **kwargs)
 
     def destroy(self, request, *args, **kwargs):
-        logger.warning(f"Deleted ticket with id={kwargs.get('pk')}")
-        try:
-            return super().destroy(request, *args, **kwargs)
-        except Exception as e:
-            logger.error(f"Error deleting ticket with id={kwargs.get('pk')}: {e}")
-            raise
+        if not request.user.is_staff:
+            raise PermissionDenied("You cannot delete tickets.")
+        return super().destroy(request, *args, **kwargs)
 
 class SeatViewSet(viewsets.ModelViewSet):
     queryset = Seat.objects.all()
--- orders/models.py
+++ orders/models.py
@@ -32,7 +32,8 @@ class Transaction(models.Model):
     status = models.CharField(max_length=20, choices=Status.choices, default=Status.PENDING)
     transaction_id = models.CharField(max_length=100, unique=True)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
-    timestamp = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f'Transaction {self.transaction_id} for Order {self.order.id}'
